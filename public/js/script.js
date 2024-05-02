2a3a40f5f0b5f67d6dcd548d0d685da43103680b
Jesus Vasquez <jesusvasquezesc7@gmail.com>
2024-05-01 21:39:57 -0400

    new function

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,12 @@ let sendBtn=document.getElementById('sendBtn');
 let textBox=document.getElementById('textbox');
 let chatContainer=document.getElementById('chatContainer');
 let user={message:""};
+
+let arrayOfPosibleMessages=[
+    {message:"hola",response:"Hola soy un chatbot"},
+    {message:"Como estas hoy?",response:"Estoy bien gracias por preguntar."},
+    {message:"Escurri",response:"Escurri"},
+];
 const sendMessage=(userMessage)=>{
     let messageElement=document.createElement('div');
     messageElement.style.textAlign="right";
@@ -10,6 +16,30 @@ const sendMessage=(userMessage)=>{
     chatContainer.appendChild(messageElement);
 
 };
+
+const chatbotMessage = (userMessage) => {
+    let chatbotMessage = "";
+    if (userMessage.trim() !== "") {
+        let result = arrayOfPosibleMessages.find(val => val.message.toLowerCase() === userMessage.toLowerCase());
+        if (result) {
+            chatbotMessage = result.response;
+        } else {
+            chatbotMessage = "No entiendo tu mensaje. Por favor ingresa un mensaje correcto";
+        }
+    } else {
+        chatbotMessage = "No enviaste nada. Por favor ingresa un mensaje";
+    }
+    let messageElement = document.createElement('div');
+    messageElement.innerHTML = "<span>Chatbot: </span>" + "<span>" + chatbotMessage + "</span>";
+    
+    setTimeout(()=>{
+        messageElement.animate([{easing:"ease-in",opacity:0.4},{opacity:1}],{duration:1000});
+        chatContainer.appendChild(messageElement);
+        chatContainer.scrollTop=chatContainer.scrollHeight;
+    }
+    ,1000);
+};
+
 sendBtn.addEventListener('click',function(e){
     let userMessage=textBox.value;
     if(userMessage==""){
@@ -19,5 +49,6 @@ sendBtn.addEventListener('click',function(e){
         userMessage.message=userMessageText;
         textBox.value="";
         sendMessage(userMessageText);
+        chatbotMessage(userMessageText);
     }
 })
