753b7f8170dd74ecdaf137ad403f3d870482ed16
Jesus Vasquez <jesusvasquezesc7@gmail.com>
2024-05-15 23:34:37 -0400

    tercer commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const ejs=require('ejs');
 const app=express();
+const port = process.env.PORT || 3000
 app.use(express.static('public'));
 app.set('view engine','ejs');
-app.listen(3000);
+app.listen(port);
 
 app.get('/',function(req,res){
     res.render('pages/index');
